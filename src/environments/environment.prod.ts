4b67f8716843f2f709b25b079144fc65f0c12df3
Unknown <luciano@lrnco.cc>
2017-01-31 00:26:30 -0200

    Delete ENV files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api_base_url: 'http://onebnb/api/v1/'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  api_base_url: 'http://localhost:3000/api/v1/'
-};
